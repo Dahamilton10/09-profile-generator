e289d0e5e49a5bbe57479fe7ed3b007e75402bda
David Hamilton <dahamilton10@yahoo.com>
2019-11-23 12:38:13 -0500

    did npm init and added axios, inquirer, and fs. also added some inquirer quesstions

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
+var inquirer = require("inquirer");
+var fs = require('fs');
+
+
 const questions = [
-  
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "userName",
+            message: "What is your github username?"
+        },
+        {
+            type: "list",
+            message: "What is your favorite color?",
+            name: "color",
+            choices: [
+                "green",
+                "blue",
+                "pink",
+                "red",
+            ]
+        }
+    ]).then(function (data) {
+
+        var filename = data.name.toLowerCase().split(' ').join('') + ".json";
+
+        fs.writeFile(filename, JSON.stringify(data, null, '\t'), function (err) {
+
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log("Success!");
+
+        });
+    });
 ];
 
 function writeToFile(fileName, data) {
- 
+
 }
 
 function init() {
-    
+
 }
 init();
